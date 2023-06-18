018aa87bf6b423360c4b7d969a7b71903b1a1db0
thinler <yyh.email@sjtu.edu.cn>
2023-06-18 21:36:21 +0800

    a_try_of_matplotlib

--- git_practice.py
+++ git_practice.py
@@ -1,2 +1,8 @@
+import matplotlib.pyplot as plt
+
+input_values=list(range(0,101,2))
 squares=[value**2 for value in range(0,101,2)]
-print(squares)
+fig, ax=plt.subplots()
+ax.plot(input_values,squares, linewidth=3)
+
+plt.show()
