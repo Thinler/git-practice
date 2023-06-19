76ac2b46ddc4235d34b756e2631cb979cd8abc9b
thinler <yyh.email@sjtu.edu.cn>
2023-06-19 13:18:13 +0800

    dic_get()

--- git_practice.py
+++ git_practice.py
@@ -1,6 +1,7 @@
-users=['admin','john','bob','sarah','alice']
-for user in users:
-    if user=='admin':
-        print("hello Admin,would you like tp see a status report")
-    else:
-        print(f"hello,{user.title()},thank you for logging")
+alien_0={
+    'color':'green',
+    'age':15,
+    'speed':'medium',
+}
+test = alien_0.get('use')
+print(test)
