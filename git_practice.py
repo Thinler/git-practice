c2ab33641c3351db1eaa471aa0fefacf23b05a76
thinler <yyh.email@sjtu.edu.cn>
2023-06-24 14:53:56 +0800

    6-24

--- git_practice.py
+++ git_practice.py
@@ -1,20 +1,4 @@
-cities={
-    'beijing':{
-        'pop':'****',
-        'country':'cn',
-        'fact':'captain',
-    },
-    'shanghai':{
-        'pop':'*****',
-        'country':'cn',
-        'fact':'modern',
-    },
-    'tianjin':{
-        'pop':'***',
-        'country':'cn',
-        'fact':'many of my friends there'
-    },
-}
-for city,cityinfo in cities.items():
-    print(f"As for the city {city.title()},the pop is {cityinfo['pop'].title()},"
-          f"the country is {cityinfo['country'].title()},the fact is that it is {cityinfo['fact'].title()}")
+test=0
+print(test)
+test+=1
+print(test)
